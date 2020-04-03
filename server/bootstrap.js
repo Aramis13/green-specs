5d5088584f90d3de0b938871944eb5d102a77b54
Nitay Kufert <nitay.k@supersonic.com>
2020-04-03 15:02:48 +0300

    nitay/analyzeRoute: naive implementation with mocks & hard coded params

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -4,34 +4,34 @@ import {log} from './utils';
 import web_routes from './routes/route';
 
 export default function (app) {
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended: true}));
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
-  // Route
-  app.use(web_routes);
-
-  // 404
-  app.use((req, res) => {
-    res.status(404).send({
-      status: 404,
-      message: 'The requested resource was not found',
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
     });
-  });
 
-  // 5xx
-  app.use((err, req, res) => {
-    log.error(err.stack);
-    const message = process.env.NODE_ENV === 'production'
-      ? 'Something went wrong, we\'re looking into it...'
-      : err.stack;
-    res.status(500).send({
-      status: 500,
-      message,
+    // Route
+    app.use(web_routes);
+
+    // 404
+    app.use((req, res) => {
+        res.status(404).send({
+            status: 404,
+            message: 'The requested resource was not found',
+        });
     });
-  });
+
+    // 5xx
+    app.use((err, req, res) => {
+        log.error(err.stack);
+        const message = process.env.NODE_ENV === 'production'
+            ? 'Something went wrong, we\'re looking into it...'
+            : err.stack;
+        res.status(500).send({
+            status: 500,
+            message,
+        });
+    });
 }
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,9 +1,45 @@
 import express from 'express';
+import {Downloader} from "../modules/downloader/downloader";
+import moment from 'moment';
+import {PythonRunner} from "../modules/pythonRunner/pythonRunner";
 
 const router = new express.Router();
 
+const downloader = new Downloader('downloaded_pictures/');
+const pythonRunner = new PythonRunner();
+
 router.get('/healthcheck', function (req, res) {
     res.json("OK");
+});
+
+router.get('/analyze', function (req, res) {
+    const picUrl = req.query.pic_url;
+    const id = req.query.id;
+
+    if (!id) res.json("Missing id param");
+    else if (!picUrl) res.json("Missing pic_url param");
+    else {
+        // download & save pic from url
+        const now = moment.now();
+        const fileName = `${id}_${now}.jpg`;
+        let pictureLocation;
+        downloader.download(picUrl, fileName, function (fileLocation) {
+            return function () {
+                console.log(`Download finished. saved to: ${fileLocation}`);
+                pictureLocation = fileLocation;
+            };
+        });
+
+        // run dandan's script
+        pythonRunner.run(pictureLocation, function (ans) {
+            // parse scripts results
+
+            // send to agam
+            res.json(ans);
+        });
+
+    }
+
 });
 
 export default router;
