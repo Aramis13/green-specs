647ece20c98309fd7550ae3e8b88ae0503b95b20
daniel <daniel.gilon@gmail.com>
2020-04-18 22:33:59 +0300

    Orig Image

--- python/API.py
+++ python/API.py
@@ -1,3 +1,5 @@
+import argparse
+
 import algo
 import cv2
 import numpy as np
@@ -25,13 +27,20 @@ def api_id():
 
     sorted_contours = algo.get_region_image_from_image(current_image)
 
-    svg_paths = response_preparation.convert_to_svg(sorted_contours)
+    shape_paths = response_preparation.convert_to_svg(sorted_contours)
 
     # Use the jsonify function from Flask to convert our list of Python dictionaries to the JSON format.
-    return jsonify(svg_paths)
+    return jsonify(shape_paths)
 
 
 if __name__ == '__main__':
-    # print('http://127.0.0.1:5000/?image_url=https://i2-prod.gazettelive.co.uk/incoming/article15432228.ece/ALTERNATES/s615/0_DRR_MGA__181118MGAklmJPG.jpg')
-    # app.run(debug=True)
-    app.run(debug=False)
+    parser = argparse.ArgumentParser(description='Process args.')
+    parser.add_argument('--debug', action='store_true')
+
+    args = parser.parse_args()
+
+    if args.debug is True:
+        # print('http://127.0.0.1:5000/?image_url=https://i2-prod.gazettelive.co.uk/incoming/article15432228.ece/ALTERNATES/s615/0_DRR_MGA__181118MGAklmJPG.jpg')
+        print('http://127.0.0.1:5000/?image_url=https://firebasestorage.googleapis.com/v0/b/green-specs.appspot.com/o/WhatsApp%20Image%202020-04-10%20at%2017.49.29.jpeg?alt=media&token=527d2725-12de-4dc0-9d2c-ef5541fda84c')
+
+    app.run(debug=args.debug)
