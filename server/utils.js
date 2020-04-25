d12b13b7ea407b21129971444115bc84fb439d04
Nitay Kufert <nitay.k@supersonic.com>
2020-04-25 19:30:50 +0300

    Added firebase DB support - read entire data and return it

--- server/utils.js
+++ server/utils.js
@@ -2,15 +2,15 @@ import morgan from 'morgan';
 import tracer from 'tracer';
 
 export const log = (() => {
-  const logger = tracer.colorConsole();
-  logger.requestLogger = morgan('dev');
-  return logger;
+    const logger = tracer.colorConsole();
+    logger.requestLogger = morgan('dev');
+    return logger;
 })();
 
 export const normalizePort = (val) => {
-  const port = parseInt(val, 10);
-  if (Number.isNaN(port)) return val;
-  if (port >= 0) return port;
-  return false;
+    const port = parseInt(val, 10);
+    if (Number.isNaN(port)) return val;
+    if (port >= 0) return port;
+    return false;
 };
 
