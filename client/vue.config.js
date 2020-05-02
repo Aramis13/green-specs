d47a9c7d9ce80cad11414e99fa38ba313ac69d1a
Idan Agam <edanagam@yahoo.com>
2020-05-02 20:54:55 +0300

    Add pwa config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "Green Specs",
+    themeColor: "#1B5E20",
+    appleMobileWebAppCapable: "yes"
+  }
 };
